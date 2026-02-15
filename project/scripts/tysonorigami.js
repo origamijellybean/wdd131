91d2469c2be71322c741e42601df3c00e42f7d22
origamijellybean <thunter@byupathway.edu>
2026-02-14 22:25:33 -0700

    main edits

--- project/scripts/tysonorigami.js
+++ project/scripts/tysonorigami.js
@@ -10,7 +10,7 @@ const navigation = document.querySelector("nav ul");
 hamburger.addEventListener("click", () => {
     navigation.classList.toggle("visible");
     hamburger.classList.toggle("visible");
-})
+});
 
 const products = [
   {
@@ -38,55 +38,54 @@ if (window.location.pathname.includes("store.html")) {
         productElement.value = product.id;
         productSelector.appendChild(productElement);
     });
+    const bookSelector = document.querySelector("#product");
+    const prices = document.querySelectorAll(".rawprice");
+    const taxes = document.querySelector(".tax");
+    const shippingCost = document.querySelector(".shipping");
+    const totalCost = document.querySelector(".total");
+    const extraForms = document.querySelectorAll(".expandedForm");
+
+    bookSelector.addEventListener('change', () => {
+        extraForms.forEach(extraForm => {
+            extraForm.classList.remove("invisible");
+        });
+        let bookSelection = products.find(book => book.id === bookSelector.value);
+        prices.forEach(price => {
+            price.textContent = `Price (before shipping and tax): $${bookSelection.price}`;
+        });
+        let tax = bookSelection.price * 0.0695;
+        taxes.textContent = `Taxes: $${tax.toFixed(2)}`;
+        let shipping = 5.00;
+        shippingCost.textContent = `Shipping: $${shipping.toFixed(2)}`;
+        let total = bookSelection.price + tax + shipping;
+        totalCost.textContent = `Total Cost: $${total.toFixed(2)}`;
+    });
+};
+if (window.location.pathname.includes("info-page.html")) {
+    const historyBtn = document.querySelector("#history");
+    const stylesBtn = document.querySelector("#styles");
+    const paperBtn = document.querySelector("#paper");
+    const designsBtn = document.querySelector("#designs");
+    const history = document.querySelector(".history");
+    const styles = document.querySelector(".styles");
+    const paper = document.querySelector(".paper");
+    const designs = document.querySelector(".designs");
+
+    historyBtn.addEventListener("click", () => {
+        historyBtn.classList.toggle("visible");
+        history.classList.toggle("visible");
+    });
+    stylesBtn.addEventListener("click", () => {
+        stylesBtn.classList.toggle("visible");
+        styles.classList.toggle("visible");
+    });
+    paperBtn.addEventListener("click", () => {
+        paperBtn.classList.toggle("visible");
+        paper.classList.toggle("visible");
+    });
+    designsBtn.addEventListener("click", () => {
+        designsBtn.classList.toggle("visible");
+        designs.classList.toggle("visible");
+    });
 }
-const bookSelector = document.querySelector("#product");
-const prices = document.querySelectorAll(".rawprice");
-const taxes = document.querySelector(".tax");
-const shippingCost = document.querySelector(".shipping");
-const totalCost = document.querySelector(".total");
-const extraForms = document.querySelectorAll(".expandedForm");
-
-bookSelector.addEventListener('change', () => {
-    extraForms.forEach(extraForm => {
-        extraForm.classList.remove("invisible");
-    });
-    let bookSelection = products.find(book => book.id === bookSelector.value);
-    prices.forEach(price => {
-        price.textContent = `Price (before shipping and tax): $${bookSelection.price}`;
-    });
-    let tax = bookSelection.price * 0.0695;
-    taxes.textContent = `Taxes: $${tax.toFixed(2)}`;
-    let shipping = 5.00;
-    shippingCost.textContent = `Shipping: $${shipping.toFixed(2)}`;
-    let total = bookSelection.price + tax + shipping;
-    totalCost.textContent = `Total Cost: $${total.toFixed(2)}`;
-});
-
-const historyBtn = document.querySelector("#history");
-const stylesBtn = document.querySelector("#styles");
-const paperBtn = document.querySelector("#paper");
-const designsBtn = document.querySelector("#designs");
-const history = document.querySelector(".history");
-const styles = document.querySelector(".styles");
-const paper = document.querySelector(".paper");
-const designs = document.querySelector(".designs");
-
-historyBtn.addEventListener("click", () => {
-    historyBtn.classList.toggle("visible");
-    history.classList.toggle("visible");
-    
-})
-stylesBtn.addEventListener("click", () => {
-    stylesBtn.classList.toggle("visible");
-    styles.classList.toggle("visible");
-})
-paperBtn.addEventListener("click", () => {
-    paperBtn.classList.toggle("visible");
-    paper.classList.toggle("visible");
-})
-designsBtn.addEventListener("click", () => {
-    designsBtn.classList.toggle("visible");
-    designs.classList.toggle("visible");
-})
-
 // Need to populate the store with items, and the form stuff. Need a list so consumer can click on items to add to cart list. When done, they need to be able to put in card information. Once done, a thank you message should be displayed, or on another page.
