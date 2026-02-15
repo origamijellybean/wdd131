bfb6d61c2d2f9a19b4eba2ce9c2c89739e8549c2
origamijellybean <thunter@byupathway.edu>
2026-02-14 19:54:08 -0700

    Almost functional form

--- project/scripts/tysonorigami.js
+++ project/scripts/tysonorigami.js
@@ -12,6 +12,34 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("visible");
 })
 
+const products = [
+  {
+    id: "or-temple-bk",
+    name: "Temple book",
+    price: 14.99
+  },
+  {
+    id: "or-ctrflr-bk",
+    name: "Creatures & Flowers book",
+    price: 12.99
+  },
+  {
+    id: "or-symbol-bk",
+    name: "Scripture symbols book",
+    price: 13.99
+  }
+];
+if (window.location.pathname.includes("store.html")) {
+    const productSelector = document.querySelector("select");
+    const form = document.querySelector("form");
+    products.forEach(product => {
+        let productElement = document.createElement("option");
+        productElement.textContent = product.name;
+        productElement.value = product.id;
+        productSelector.appendChild(productElement);
+    });
+}
+
 
 const historyBtn = document.querySelector("#history");
 const stylesBtn = document.querySelector("#styles");
